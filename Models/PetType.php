1830d4f5dbe12ed40d21deb2946115766af5ce93
marcotarchini <tarchinimarco@gmail.com>
2022-10-28 18:49:14 -0300

    agrego pettype y schema

---
+++ Models/PetType.php
@@ -0,0 +1,32 @@
+<?php
+    namespace Models;
+
+    class PetType
+    {
+        private $id;
+        private $type;
+
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        public function getType()
+        {
+                return $this->type;
+        }
+
+        public function setType($type)
+        {
+                $this->type = $type;
+
+                return $this;
+        }
+    }
